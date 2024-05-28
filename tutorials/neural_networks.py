87adb99c5de749ce30fef07ad9b4703523d1c750
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2024-05-28 23:23:56 +0300

    Neural networks placeholder

---
+++ tutorials/neural_networks.py
@@ -0,0 +1,31 @@
+import marimo
+
+__generated_with = "0.6.8"
+app = marimo.App()
+
+
+@app.cell
+def __():
+    import marimo as mo
+    return mo,
+
+
+@app.cell
+def __(mo):
+    mo.md(
+        rf"""
+        # Neural network models
+
+        **TODO**, please check back later.
+        """
+    )
+    return
+
+
+@app.cell
+def __():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
